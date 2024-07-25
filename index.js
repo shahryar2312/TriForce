870b9d466790e17f76f69f36474fe9e1795fd9d3
CodeNameJaemin <159160454+CodeNameJaemin@users.noreply.github.com>
2024-07-26 00:58:37 +0100

    J-Batch: 0.3.7
    final update

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
         updateGallery();
     });
 });
+
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menuList = document.getElementById('menu-list');
+    if (menuList.style.display === 'flex') {
+        menuList.style.display = 'none';
+    } else {
+        menuList.style.display = 'flex';
+    }
+});
